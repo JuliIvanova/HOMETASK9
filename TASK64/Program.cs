acbce6afcb7f5607b80c263e236ed3a34f0019ae
«Julia <mir275@yandex.ru>
2022-12-14 13:10:55 +0300

    TASK64

---
+++ TASK64/Program.cs
@@ -0,0 +1,18 @@
+﻿int n=InputNumbers("Введите n:");
+int count=2;
+PrintNumber(n,count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+ if (count>n)return;
+ PrintNumber(n,count+1);
+ Console.WriteLine(count+",");
+}
+
+int InputNumbers(string input)
+{
+    Console.WriteLine(input);
+    int output=Convert.ToInt32(Console.ReadLine());
+    return output;
+}
